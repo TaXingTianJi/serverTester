541520a11cfe4e754a23858a0547de311938f9ea
zerak <Kingbug2010@126.com>
2016-04-01 17:48:36 +0800

    protocol

--- serverFrameworkTest.py
+++ serverFrameworkTest.py
@@ -1,8 +1,17 @@
 #server framework tester
 import socket
 import time
+import struct
 
-address = ('127.0.0.1',4150)
+header = '0x05'
+cmd = 1002
+length = 3
+data = 'abc'
+
+string = struct.pack('!HHs',cmd,length,data)
+
+#address = ('127.0.0.1',4150)
+address = ('127.0.0.1',8888)
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect(address)
 
@@ -12,8 +21,9 @@ while 1:
   #s.send('\ncmd len data\n cmd1 len1 data1\n cmd2 len2 data2\n')
   #t0 = time.clock()
   t0 = time.time()
-  s.send('\n IDENTIFY ')
-  time.sleep(0.1)
+  #s.send('\n a b c')
+  s.send(string)
+  time.sleep(10.1)
 
   data = s.recv(1024)
   if not data:
