29fcc53a4cbc91229d26bd22f1c00f81e8acf00b
zerak <Kingbug2010@126.com>
2016-04-07 17:50:44 +0800

    test protocol_v1

--- serverFrameworkTest.py
+++ serverFrameworkTest.py
@@ -3,34 +3,39 @@ import socket
 import time
 import struct
 
-header = '0x05'
-cmd = 1002
-length = 3
-data = 'abc'
+header = 0x05	# 1 byte
+cmd = 1002	    # 4 byte
+length = 3	    # 4 bytexz
+data = 'abc'	# len byte
 
-string = struct.pack('!HHs',cmd,length,data)
+string = struct.pack('!b2i3s',header,cmd,length,data)
+
+# send of piece
+str1 = struct.pack('!b2i',header,cmd,length)
+str2 = struct.pack('!3s',data)
 
 #address = ('127.0.0.1',4150)
 address = ('127.0.0.1',8888)
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect(address)
 
-s.send('  V2') 	#protocol v1
+s.send('  V1') 	#protocol v1
 
 while 1:
-  #s.send('\ncmd len data\n cmd1 len1 data1\n cmd2 len2 data2\n')
-  #t0 = time.clock()
   t0 = time.time()
-  #s.send('\n a b c')
+  #s.send('\nabc')
+
   s.send(string)
-  time.sleep(10.1)
+  time.sleep(2)
+  s.send(str1)
+  time.sleep(2)
+  s.send(str2)
 
   data = s.recv(1024)
   if not data:
     break
   print data
-  #print "ping %d" %(time.clock() - t0)
   print "ping %d %s" %(time.time()*1000 - t0*1000,'ms')
- 
+
 
 s.close()
